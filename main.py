6b28d0c9bf20347e0359e298f708700899cb4398
titoiriepal <titoiriepal@hotmail.com>
2026-02-03 14:22:38 +0100

    Models.py and main.py created for solution

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
-import init_django_orm  # noqa: F401
-
 from django.db.models import QuerySet
 
+from db.models import Genre, Actor
 
-def main() -> QuerySet:
-    pass
+
+def main() -> QuerySet[Actor]:
+    Genre.objects.create(name="Western")
+    Genre.objects.create(name="Action")
+    Genre.objects.create(name="Dramma")
+
+    Actor.objects.create(first_name="George", last_name="Klooney")
+    Actor.objects.create(first_name="Kianu", last_name="Reaves")
+    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
+    Actor.objects.create(first_name="Will", last_name="Smith")
+    Actor.objects.create(first_name="Jaden", last_name="Smith")
+    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+
+    genre_to_update = Genre.objects.get(name="Dramma")
+    genre_to_update.name = "Drama"
+    genre_to_update.save()
+
+    actor_to_update = Actor.objects.get(first_name="George", last_name="Klooney")
+    actor_to_update.last_name = "Clooney"
+    actor_to_update.save()
+
+    actor_to_update = Actor.objects.get(first_name="Kianu", last_name="Reaves")
+    actor_to_update.first_name = "Keanu"
+    actor_to_update.last_name = "Reeves"
+    actor_to_update.save()
+
+    genre_to_delete = Genre.objects.get(name="Action")
+    genre_to_delete.delete()
+
+    Actor.objects.filter(first_name="Scarlett").delete()
+
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
+
