1763524aaf96b7bcae0c869d5b385de7c1bad452
titoiriepal <titoiriepal@hotmail.com>
2026-02-03 14:30:25 +0100

    Blank space around operator str in models.py:15:25

--- db/models.py
+++ db/models.py
@@ -12,5 +12,5 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
-    def __str__(self) ->str:
+    def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
